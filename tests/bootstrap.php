0207e15c76ba6db4d38330ea9886547932f9c0cc
Thomas Rabaix <thomas.rabaix@ekino.com>
2013-08-05 11:03:50 +0200

    Fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
- * This file is part of the Sonata package.
+ * this file is part of the symfony package.
  *
- * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ * (c) fabien potencier <fabien.potencier@symfony-project.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * for the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-if (file_exists($file = __DIR__.'/../autoload.php')) {
+if (file_exists($file = __DIR__.'/autoload.php')) {
     require_once $file;
-} elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
     require_once $file;
 }
