9b1c702ca3816e1c29c0d8c4d70876b1dcd62642
Jérôme FIX <jerome.fix@zapoyok.info>
2015-04-03 06:26:30 +0200

    Unit tests

---
+++ tests/Mocks/DatabasePlatformMock.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace  Sonata\Doctrine\Tests\Mocks;
+
+use Doctrine\DBAL\DBALException;
+
+/**
+ * Mock class for DatabasePlatform.
+ */
+class DatabasePlatformMock extends \Doctrine\DBAL\Platforms\AbstractPlatform
+{
+    /**
+     * @var string
+     */
+    private $_sequenceNextValSql = "";
+
+    /**
+     * @var bool
+     */
+    private $_prefersIdentityColumns = true;
+
+    /**
+     * @var bool
+     */
+    private $_prefersSequences = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prefersIdentityColumns()
+    {
+        return $this->_prefersIdentityColumns;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prefersSequences()
+    {
+        return $this->_prefersSequences;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSequenceNextValSQL($sequenceName)
+    {
+        return $this->_sequenceNextValSql;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBooleanTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIntegerTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBigIntTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSmallIntTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _getCommonIntegerTypeDeclarationSQL(array $columnDef)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVarcharTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClobTypeDeclarationSQL(array $field)
+    {
+    }
+
+    /* MOCK API */
+
+    /**
+     * @param bool $bool
+     */
+    public function setPrefersIdentityColumns($bool)
+    {
+        $this->_prefersIdentityColumns = $bool;
+    }
+
+    /**
+     * @param bool $bool
+     */
+    public function setPrefersSequences($bool)
+    {
+        $this->_prefersSequences = $bool;
+    }
+
+    /**
+     * @param string $sql
+     */
+    public function setSequenceNextValSql($sql)
+    {
+        $this->_sequenceNextValSql = $sql;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'mock';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function initializeDoctrineTypeMappings()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlobTypeDeclarationSQL(array $field)
+    {
+        throw DBALException::notSupported(__METHOD__);
+    }
+}
---
+++ tests/Query/SortableNullsWalkerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Doctrine\Tests\Query;
+
+use Doctrine\ORM\Query;
+use Sonata\Doctrine\Query\SortableNullsWalker;
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Sonata\Doctrine\Tests\Mocks\DriverMock;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\MySqlPlatform;
+
+class SortableNullsWalkerTest extends \PHPUnit_Framework_TestCase
+{
+    protected function getTestEntityManager(AbstractPlatform $platform)
+    {
+        $config = new \Doctrine\ORM\Configuration();
+        $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
+        $config->setQueryCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
+        $config->setProxyDir(__DIR__.'/Proxies');
+        $config->setProxyNamespace('DoctrineExtensions\Tests\Proxies');
+        $config->setAutoGenerateProxyClasses(true);
+        $config->setMetadataDriverImpl($config->newDefaultAnnotationDriver(__DIR__.'/../Entities'));
+
+        $conn = array(
+            'driverClass' => 'Sonata\Doctrine\Tests\Mocks\DriverMock',
+            'platform' => $platform,
+        );
+        $entityManager = \Doctrine\ORM\EntityManager::create($conn, $config);
+
+        return $entityManager;
+    }
+
+    public function testWalkOrderByItemNullsFirst()
+    {
+        $entity = 'Sonata\Doctrine\Tests\Entities\BlogPost';
+
+        // MySql
+        $dql = "SELECT p FROM {$entity} p ORDER BY p.position ASC";
+        $q = $this->getTestEntityManager(new MySqlPlatform())->createQuery($dql)
+                ->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, 'Sonata\Doctrine\Query\SortableNullsWalker')
+                ->setHint("sortableNulls.fields", array("p.position" => SortableNullsWalker::NULLS_FIRST))
+        ;
+
+        $sqlToBeConfirmed = "SELECT b0_.id AS id0, b0_.title AS title1, b0_.position AS position2 FROM BlogPost b0_ ORDER BY b0_.position ASC";
+
+        $sqlGenerated =  $q->getSql();
+
+        $this->assertEquals(
+            $sqlToBeConfirmed,
+            $sqlGenerated,
+            sprintf('"%s" is not equal of "%s"', $sqlGenerated, $sqlToBeConfirmed)
+        );
+
+        // Postgresql
+        $dql = "SELECT p FROM {$entity} p ORDER BY p.position ASC";
+        $q = $this->getTestEntityManager(new PostgreSqlPlatform())->createQuery($dql)
+                ->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, 'Sonata\Doctrine\Query\SortableNullsWalker')
+                ->setHint("sortableNulls.fields", array("p.position" => SortableNullsWalker::NULLS_FIRST))
+        ;
+
+        $sqlToBeConfirmed = "SELECT b0_.id AS id0, b0_.title AS title1, b0_.position AS position2 FROM BlogPost b0_ ORDER BY b0_.position ASC NULLS FIRST";
+
+        $sqlGenerated =  $q->getSql();
+
+        $this->assertEquals(
+            $sqlToBeConfirmed,
+            $sqlGenerated,
+            sprintf('"%s" is not equal of "%s"', $sqlGenerated, $sqlToBeConfirmed)
+        );
+    }
+
+    public function testWalkOrderByItemNullsLast()
+    {
+        $entity = 'Sonata\Doctrine\Tests\Entities\BlogPost';
+
+        // MySql
+        $dql = "SELECT p FROM {$entity} p ORDER BY p.position ASC";
+        $q = $this->getTestEntityManager(new MySqlPlatform())->createQuery($dql)
+                ->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, 'Sonata\Doctrine\Query\SortableNullsWalker')
+                ->setHint("sortableNulls.fields", array("p.position" => SortableNullsWalker::NULLS_LAST))
+        ;
+
+        $sqlToBeConfirmed = "SELECT b0_.id AS id0, b0_.title AS title1, b0_.position AS position2 FROM BlogPost b0_ ORDER BY b0_.position ASC";
+
+        $sqlGenerated =  $q->getSql();
+
+        $this->assertEquals(
+            $sqlToBeConfirmed,
+            $sqlGenerated,
+            sprintf('"%s" is not equal of "%s"', $sqlGenerated, $sqlToBeConfirmed)
+        );
+
+        // Postgresql
+        $dql = "SELECT p FROM {$entity} p ORDER BY p.position ASC";
+        $q = $this->getTestEntityManager(new PostgreSqlPlatform())->createQuery($dql)
+                ->setHint(Query::HINT_CUSTOM_OUTPUT_WALKER, 'Sonata\Doctrine\Query\SortableNullsWalker')
+                ->setHint("sortableNulls.fields", array("p.position" => SortableNullsWalker::NULLS_LAST))
+        ;
+
+        $sqlToBeConfirmed = "SELECT b0_.id AS id0, b0_.title AS title1, b0_.position AS position2 FROM BlogPost b0_ ORDER BY b0_.position ASC NULLS LAST";
+
+        $sqlGenerated =  $q->getSql();
+
+        $this->assertEquals(
+            $sqlToBeConfirmed,
+            $sqlGenerated,
+            sprintf('"%s" is not equal of "%s"', $sqlGenerated, $sqlToBeConfirmed)
+        );
+    }
+}
