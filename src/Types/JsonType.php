072e53621efe35feed31f4eb7da73672b8e5640f
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-09 23:51:00 +0100

    Comply with php7 cs fixer rules

--- src/Types/JsonType.php
+++ src/Types/JsonType.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -19,7 +21,7 @@ use Doctrine\DBAL\Types\Type;
  */
 class JsonType extends Type
 {
-    const JSON = 'json';
+    public const JSON = 'json';
 
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
