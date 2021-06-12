fc760c69f64615f976c08b6d3c97094e4e8ad67d
Jordi <jordism91@gmail.com>
2021-06-12 11:18:49 +0200

    Add ClearableManagerInterface

---
+++ src/Model/ClearableManagerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\Doctrine\Model;
+
+/**
+ * @author Jordi Sala <jordism91@gmail.com>
+ *
+ * @phpstan-template T of object
+ */
+interface ClearableManagerInterface
+{
+    /**
+     * Clears the object manager
+     */
+    public function clear(?string $objectName = null): void;
+}
