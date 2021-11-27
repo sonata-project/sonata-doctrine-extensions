15b7807f6282affde191c9adf5a094457f2c607d
Jordi Sala Morales <jordism91@gmail.com>
2021-11-27 17:05:22 +0100

    Remove explicit error throw and rely on deprecated annotation

--- src/Bridge/Symfony/Bundle/SonataDoctrineBundle.php
+++ src/Bridge/Symfony/Bundle/SonataDoctrineBundle.php
@@ -16,12 +16,6 @@ namespace Sonata\Doctrine\Bridge\Symfony\Bundle;
 use Sonata\Doctrine\Bridge\Symfony\SonataDoctrineBundle as ForwardCompatibleSonataDoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-@trigger_error(sprintf(
-    'The %s\SonataDoctrineBundle class is deprecated since sonata-project/doctrine-extensions 1.9, to be removed in version 2.0. Use %s instead.',
-    __NAMESPACE__,
-    ForwardCompatibleSonataDoctrineBundle::class
-), \E_USER_DEPRECATED);
-
 // @phpstan-ignore-next-line
 if (false) {
     /**
